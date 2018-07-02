3a1f0347e2256b51b0ecbab101a80ecc39c3f50d
Carib Guerra <yes@carib.codes>
2018-07-01 21:08:49 -0400

    Initial commit

---
+++ TextAdventure/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+	
+	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+
+	State state;
+
+	// Use this for initialization
+	void Start () {
+		textComponent.text = ("Welcome to the future...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
