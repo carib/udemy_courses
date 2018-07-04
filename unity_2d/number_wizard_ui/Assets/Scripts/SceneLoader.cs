59eed54828f34c67f7ebf9f1207f7f34ce62d62c
Carib Guerra <yes@carib.codes>
2018-07-03 23:00:03 -0400

    Complete section 4

--- unity_2d/number_wizard_ui/Assets/Scripts/SceneLoader.cs
+++ unity_2d/number_wizard_ui/Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	public void LoadNextScene()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void LoadStartMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 }
